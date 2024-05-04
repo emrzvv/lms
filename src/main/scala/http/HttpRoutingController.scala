7a01f6c37593d8151f21fe7f60c8d8b7ba5d9b2c
emrzvv <e.mrzvv@gmail.com>
2024-05-05 00:50:49 +0300

    wip

--- src/main/scala/http/HttpRoutingController.scala
+++ src/main/scala/http/HttpRoutingController.scala
@@ -2,10 +2,11 @@ package http
 
 import component.{ActorSystemComponent, Repositories}
 import http.auth.JwtSecurity
-import http.controller.UserController
+import http.controller.{CourseController, UserController}
 import utils.Serializers
 
 trait HttpRoutingController extends HttpBaseController
-  with UserController {
+  with UserController
+  with CourseController {
   this: Repositories with ActorSystemComponent with Serializers with HttpRoute with JwtSecurity =>
 }
